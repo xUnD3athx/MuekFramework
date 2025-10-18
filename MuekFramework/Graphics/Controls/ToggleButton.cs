7fa60e7273d19f4d3e0c73b6b666c09f26f1e1e8
xUnD3athx <1132668457@qq.com>
2025-10-18 23:09:14 +0800

    Add toggle button

---
+++ MuekFramework/Graphics/Controls/ToggleButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+
+namespace MuekFramework.Graphics.Controls;
+
+public class ToggleButton : Button
+{
+    public bool IsChecked { get; set; }
+    public Muek.MuekColor CheckedColor { get; set; }
+    public Vector2 CheckedScale { get; set; } = Vector2.One;
+    public event ButtonDelegate? OnCheck;
+    public event ButtonDelegate? OnUncheck;
+    public ToggleButton(Muek.MuekColor color, int width, int height, int x = 0, int y = 0) : base(color, width, height, x, y)
+    {
+        CheckedColor = Color;
+        OnRender += (c =>
+        {
+            if (IsChecked) OnChecked();
+            else OnUnchecked();
+            Console.WriteLine(IsChecked);
+        });
+    }
+
+    protected override void OnPointerClicked()
+    {
+        base.OnPointerClicked();
+        IsChecked = !IsChecked;
+    }
+
+    protected override void OnLeave()
+    {
+        IsHovering = false;
+        RenderLayer = 0;
+        if (!IsChecked) base.OnLeave();
+        
+    }
+
+    public void OnChecked()
+    {
+        var targetColor = CheckedColor;
+        var targetScale = CheckedScale;
+        TransitionTo(targetColor, AnimationSpeed);
+        TransitionTo(targetScale, AnimationSpeed);
+        OnCheck?.Invoke();
+    }
+
+    public void OnUnchecked()
+    {
+        OnUncheck?.Invoke();
+    }
+    
+}
