defa9f866a98ec325470354d0ede9846b4af8b79
xUnD3athx <1132668457@qq.com>
2025-10-20 14:41:53 +0800

    Update sample button to center

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,7 +7,7 @@ var count = 0;
 var customText = new Text("0 click")
 {
     FontSize = 24f,
-    ContentPosition = Muek.ContentPosition.Center
+    ContentPosition = Muek.ContentPosition.Center,
 };
 
 var window = new MuekWindow("MuekFramework Sample", 1000, 800);
@@ -16,12 +16,13 @@ var mainPanel = new Panel(Muek.MuekColors.White, 800, 600, 100, 100)
     BorderRadius = new Vector2(10F),
     IsAnimationDisabled = false,
     AnimationSpeed = .05F,
-    HoverScale = new Vector2(1.05F)
+    HoverScale = new Vector2(1.05F),
+    ContentPosition = Muek.ContentPosition.Center
 };
 var helloButton = new Button(Muek.MuekColors.Muek, 200, 200)
 {
     HoverColor = Muek.MuekColors.LightMuek,
-    PressedColor = Muek.MuekColors.DarkMuek,
+    PressedColor = Muek.MuekColors.DarkMuek
 };
 
 helloButton.OnClick += () =>
